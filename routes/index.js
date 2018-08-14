f2668bfa51fbadf7c929a70417c87483b7ba4eaf
root <orlando.dilmar.gareca.pena@gmail.com>
2018-08-14 15:36:29 -0400

    pruebas con docker

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HOLA ESTO ES UN MENSAJE DESDE DOCKER' });
 });
 
 module.exports = router;
